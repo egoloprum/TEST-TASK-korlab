46fc5a3831d3393baa511918458962a2a0a21aec
egoloprum <bayraa093@gmail.com>
2025-12-18 02:41:26 +0300

    initial commit and settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  turbopack: {},
 
-export default nextConfig;
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.('.svg')
+    )
+
+    if (!fileLoaderRule) {
+      return config
+    }
+
+    const resourceQueryNot = fileLoaderRule.resourceQuery?.not ?? []
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...resourceQueryNot, /url/] },
+        use: ['@svgr/webpack']
+      }
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+    return config
+  }
+}
+
+export default nextConfig
