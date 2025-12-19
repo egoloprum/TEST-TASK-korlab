6f6c1480423d72a8db9f7fac77afb1d825776966
egoloprum <bayraa093@gmail.com>
2025-12-19 09:17:24 +0300

    fix: path navigator on header

--- src/widgets/Contacts/ContactsSection.tsx
+++ src/widgets/Contacts/ContactsSection.tsx
@@ -2,7 +2,9 @@ import { ContactsForm } from '@/features/Contacts'
 
 export const ContactsSection = ({}) => {
   return (
-    <section className="space-y-5 lg:space-y-10 my-10 lg:my-15 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+    <section
+      className="space-y-5 lg:space-y-10 my-10 lg:my-15 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3"
+      id="contact">
       <div className="gradient-red-to-blue p-5 rounded-lg h-full w-full min-h-60 md:min-h-80">
         <h3 className="font-h5 md:font-h4 lg:font-h3 text-black font-medium uppercase h-full">
           Хотите узнать больше, рассчитать стоимость или забрать устройство на
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import { Button } from '@/shared/components'
 import { Logo } from '@/shared/logos'
 import { Menu } from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export const Header = ({}) => {
+  const pathname = usePathname()
+
+  console.log({ pathname })
+
   return (
     <header className="flex justify-between items-start lg:items-center xl:items-end">
       <div className="flex gap-4 md:gap-8 lg:gap-0 xl:gap-12 items-end flex-col sm:flex-row lg:flex-col xl:flex-row">
@@ -21,23 +28,38 @@ export const Header = ({}) => {
         <Menu className="text-(--color-blue)" />
       </Button>
       <nav className="hidden lg:flex xl:gap-8 lg:items-center xl:items-end">
-        <Button variant="ghost" href="/technology" className="font-p5">
-          Технология
+        <Button variant="ghost" href="/technology" className="font-p5 relative">
+          {pathname === '/technology' && (
+            <span className="w-2 h-2 bg-blue aspect-square absolute left-1/2 translate-x--1/2 -top-1.25"></span>
+          )}
+          <span>Технология</span>
         </Button>
-        <Button variant="ghost" href="/" className="font-p5">
-          Платформа управления
+        <Button variant="ghost" href="/" className="font-p5 relative">
+          {pathname === '/platform' && (
+            <span className="w-2 h-2 bg-blue aspect-square absolute left-1/2 translate-x--1/2 -top-1.25"></span>
+          )}
+          <span>Платформа управления</span>
         </Button>
-        <Button variant="ghost" href="/" className="font-p5">
-          тест-драйв
+        <Button variant="ghost" href="/" className="font-p5 relative">
+          {pathname === '/test-drive' && (
+            <span className="w-2 h-2 bg-blue aspect-square absolute left-1/2 translate-x--1/2 -top-1.25"></span>
+          )}
+          <span>тест-драйв</span>
         </Button>
-        <Button variant="ghost" href="/" className="font-p5">
-          Наша сеть
+        <Button variant="ghost" href="/" className="font-p5 relative">
+          {pathname === '/net' && (
+            <span className="w-2 h-2 bg-blue aspect-square absolute left-1/2 translate-x--1/2 -top-1.25"></span>
+          )}
+          <span>Наша сеть</span>
         </Button>
         <Button
           variant="ghost"
-          className="text-(--color-blue) font-p5 pr-0"
-          href="/">
-          Оставить заявку
+          className="text-(--color-blue) font-p5 relative pr-0"
+          href="#contact">
+          {pathname.includes('#contact') && (
+            <span className="w-2 h-2 bg-blue aspect-square absolute left-1/2 translate-x--1/2 -top-1.25"></span>
+          )}
+          <span>Оставить заявку</span>
         </Button>
       </nav>
     </header>
