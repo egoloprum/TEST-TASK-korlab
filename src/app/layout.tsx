66ac4af318315a69bc54f46d0d7252b293a7a323
egoloprum <bayraa093@gmail.com>
2025-12-19 07:12:36 +0300

    fix: export group

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Footer } from '@/widgets/Footer'
+import { Header } from '@/widgets/Header'
 
 export const metadata: Metadata = {
   title: 'Korlab Test Task',
@@ -13,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased p-5 md:p-7 lg:p-9">{children}</body>
+      <body className="antialiased p-5 md:p-7 lg:p-9">
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,25 @@
-import { ClientsScroll } from '@/widgets/ClientsScroll'
-import { ContactsSection } from '@/widgets/Contacts'
-import { DepartmentsSection } from '@/widgets/Departments'
-import { FeedbacksSection } from '@/widgets/Feedbacks'
-import { Footer } from '@/widgets/Footer'
-import { Header } from '@/widgets/Header'
-import { HeroSection } from '@/widgets/Hero'
-import { MonitoringSection } from '@/widgets/Monitoring'
-import { SpecialistsSection } from '@/widgets/Specialists'
-import { TechnologySection } from '@/widgets/Technology'
+import {
+  ClientsScroll,
+  ContactsSection,
+  DepartmentsSection,
+  FeedbacksSection,
+  HeroSection,
+  MonitoringSection,
+  SpecialistsSection,
+  TechnologySection
+} from '@/widgets/(main)'
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main className="my-10">
-        <HeroSection />
-        <ClientsScroll />
-        <TechnologySection />
-        <MonitoringSection />
-        <SpecialistsSection />
-        <FeedbacksSection />
-        <DepartmentsSection />
-        <ContactsSection />
-      </main>
-      <Footer />
-    </>
+    <main className="my-10">
+      <HeroSection />
+      <ClientsScroll />
+      <TechnologySection />
+      <MonitoringSection />
+      <SpecialistsSection />
+      <FeedbacksSection />
+      <DepartmentsSection />
+      <ContactsSection />
+    </main>
   )
 }
